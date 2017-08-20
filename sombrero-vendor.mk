956cb097918a2e3ca6f6aced0fafeff2a54d362d
DerTeufel <dominik-kassel@gmx.de>
2017-08-20 16:20:55 +0200

    init

---
+++ sombrero-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blocks/sombrero/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blocks/sombrero/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blocks/sombrero/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blocks/sombrero/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blocks/sombrero/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blocks/sombrero/vendor,system/vendor)
