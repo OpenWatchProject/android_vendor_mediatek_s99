8d0a7306743d1a907cffc985583f93e537fde647
DerTeufel <dominik-kassel@gmx.de>
2017-09-12 14:48:30 +0200

    move everything to 'vendor'

--- sombrero-vendor.mk
+++ sombrero-vendor.mk
@@ -1,9 +1,1 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blocks/sombrero/lib,system/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blocks/sombrero/bin,system/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blocks/sombrero/etc,system/etc)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blocks/sombrero/xbin,system/xbin)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blocks/sombrero/vendor,system/vendor)
